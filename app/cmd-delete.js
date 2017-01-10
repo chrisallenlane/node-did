290b50a67279c7d0c4fe9ea9b73f36aa52759411
Sparsh Yadav <sparshy@gmail.com>
2017-01-10 20:18:16 +0530

    Added support for deleting multiple rows at once

--- app/cmd-delete.js
+++ app/cmd-delete.js
@@ -1,9 +1,9 @@
 module.exports = function(config, options, db, callback) {
 
-  const id    = options['<id>'];
-  const query = 'DELETE FROM Log WHERE id = ?';
+  const id    = options['<id>'].join();
+  const query = `DELETE FROM Log WHERE id IN (${id})`;
 
-  db.run(query, id, function(err) {
+  db.run(query, function(err) {
 
     // fail if an error occured
     if (err) { return callback(err); }
